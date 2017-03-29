f5d6738a246a3135120cd93d9b14f112d0d0c7ba
Qiang Yu <yuq825@gmail.com>
2017-03-29 18:15:44 +0800

    drm/lima: add gpu per-ip init/fini framework
    Signed-off-by: Qiang Yu <yuq825@gmail.com>

---
+++ drivers/gpu/drm/lima/lima_mmu.c
@@ -0,0 +1,11 @@
+#include "lima.h"
+
+int lima_mmu_init(struct lima_mmu *mmu)
+{
+	return 0;
+}
+
+void lima_mmu_fini(struct lima_mmu *mmu)
+{
+
+}
---
+++ drivers/gpu/drm/lima/lima_pp.c
@@ -0,0 +1,11 @@
+#include "lima.h"
+
+int lima_pp_init(struct lima_pp *pp)
+{
+	return 0;
+}
+
+void lima_pp_fini(struct lima_pp *pp)
+{
+	
+}
