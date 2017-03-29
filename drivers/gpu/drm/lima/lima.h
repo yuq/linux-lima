2ce9312cf8f3deda23abae71ef21b39cd7e5ec85
Qiang Yu <yuq825@gmail.com>
2017-03-29 18:15:44 +0800

    drm/lima: add gpu per-ip init/fini framework
    Signed-off-by: Qiang Yu <yuq825@gmail.com>

--- drivers/gpu/drm/lima/lima.h
+++ drivers/gpu/drm/lima/lima.h
@@ -27,16 +27,81 @@ enum lima_gpu_type {
 	GPU_MALI400 = 0,
 };
 
+struct lima_device;
+
+#define LIMA_IP_MAX_NAME_LEN 32
+
+struct lima_ip {
+	struct lima_device *dev;
+	char name[LIMA_IP_MAX_NAME_LEN];
+	void __iomem *iomem;
+	int irq;
+};
+
+struct lima_pmu {
+	struct lima_ip ip;
+};
+
+struct lima_l2_cache {
+	struct lima_ip ip;
+};
+
+struct lima_mmu {
+	struct lima_ip ip;
+};
+
+struct lima_gp {
+	struct lima_ip ip;
+	struct lima_mmu *mmu;
+};
+
+struct lima_pp {
+	struct lima_ip ip;
+	struct lima_mmu *mmu;
+};
+
+#define LIMA_MAX_PP 4
+
 struct lima_device {
 	struct device *dev;
 	struct drm_device *ddev;
 	struct platform_device *pdev;
 
 	enum lima_gpu_type gpu_type;
-	u32 num_pp;
+
+	struct clk *clk_bus;
+	struct clk *clk_gpu;
+	struct reset_control *reset;
+
+	struct lima_pmu *pmu;
+
+	struct lima_l2_cache *l2_cache;
+
+	struct lima_mmu *mmu[LIMA_MAX_PP + 1];
+	int num_mmu;
+
+	struct lima_gp *gp;
+
+	struct lima_pp *pp[LIMA_MAX_PP];
+	int num_pp;
 };
 
 int lima_device_init(struct lima_device *ldev, struct drm_device *dev);
 void lima_device_fini(struct lima_device *ldev);
+
+int lima_pmu_init(struct lima_pmu *pmu);
+void lima_pmu_fini(struct lima_pmu *pmu);
+
+int lima_l2_cache_init(struct lima_l2_cache *l2_cache);
+void lima_l2_cache_fini(struct lima_l2_cache *l2_cache);
+
+int lima_mmu_init(struct lima_mmu *mmu);
+void lima_mmu_fini(struct lima_mmu *mmu);
+
+int lima_gp_init(struct lima_gp *gp);
+void lima_gp_fini(struct lima_gp *gp);
+
+int lima_pp_init(struct lima_pp *pp);
+void lima_pp_fini(struct lima_pp *pp);
 
 #endif
---
+++ drivers/gpu/drm/lima/lima_pmu.c
@@ -0,0 +1,11 @@
+#include "lima.h"
+
+int lima_pmu_init(struct lima_pmu *pmu)
+{
+	return 0;
+}
+
+void lima_pmu_fini(struct lima_pmu *pmu)
+{
+
+}
