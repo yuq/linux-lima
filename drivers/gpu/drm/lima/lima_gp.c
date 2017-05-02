81934e075cad0dabed1e4b8a965da6dcfda2a87f
Qiang Yu <yuq825@gmail.com>
2017-05-02 14:26:22 +0800

    drm/lima: add wait fence ioctl
    Signed-off-by: Qiang Yu <yuq825@gmail.com>

--- drivers/gpu/drm/lima/lima_gp.c
+++ drivers/gpu/drm/lima/lima_gp.c
@@ -1,7 +1,18 @@
 #include "lima.h"
 
+static int lima_gp_start_task(void *data, struct lima_sched_task *task)
+{
+	struct lima_gp *gp = data;
+
+	DRM_INFO("lima start task gp %s\n", gp->ip.name);
+	dma_fence_signal(task->fence);
+	return 0;
+}
+
 int lima_gp_init(struct lima_gp *gp)
 {
+	gp->pipe.start_task = lima_gp_start_task;
+	gp->pipe.data = gp;
 	return 0;
 }
 
--- drivers/gpu/drm/lima/lima_sched.c
+++ drivers/gpu/drm/lima/lima_sched.c
@@ -1,3 +1,5 @@
+#include <linux/kthread.h>
+
 #include "lima.h"
 
 struct lima_fence {
@@ -100,6 +102,7 @@ int lima_sched_task_add_dep(struct lima_sched_task *task, struct dma_fence *fenc
 
 int lima_sched_task_queue(struct lima_sched_pipe *pipe, struct lima_sched_task *task)
 {
+	unsigned long saved_flags;
 	struct lima_fence *fence;
 
 	fence = kzalloc(sizeof(*fence), GFP_KERNEL);
@@ -107,33 +110,176 @@ int lima_sched_task_queue(struct lima_sched_pipe *pipe, struct lima_sched_task *
 		return -ENOMEM;
 	fence->pipe = pipe;
 
-	mutex_lock(&pipe->lock);
+	spin_lock_irqsave(&pipe->lock, saved_flags);
 
 	dma_fence_init(&fence->base, &lima_fence_ops, &pipe->fence_lock,
 		       pipe->fence_context, ++pipe->fence_seqno);
 	task->fence = &fence->base;
 
+	/* for caller usage of the fence, otherwise pipe worker 
+	 * may consumed the fence */
+	dma_fence_get(task->fence);
+
 	list_add_tail(&task->list, &pipe->queue);
 
-	mutex_unlock(&pipe->lock);
+	spin_unlock_irqrestore(&pipe->lock, saved_flags);
+
+	wake_up(&pipe->worker_wait);
 	return 0;
 }
 
-void lima_sched_pipe_init(struct lima_sched_pipe *pipe, const char *name)
+static struct lima_sched_task *lima_sched_pipe_get_task(struct lima_sched_pipe *pipe)
 {
+	unsigned long saved_flags;
+	struct lima_sched_task *task;
+
+	spin_lock_irqsave(&pipe->lock, saved_flags);
+	task = list_first_entry_or_null(&pipe->queue, struct lima_sched_task, list);
+	spin_unlock_irqrestore(&pipe->lock, saved_flags);
+	return task;
+}
+
+static int lima_sched_pipe_worker(void *param)
+{
+	struct lima_sched_pipe *pipe = param;
+	struct lima_sched_task *task;
+
+	while (!kthread_should_stop()) {
+		int i;
+		unsigned long saved_flags;
+
+		wait_event_interruptible(pipe->worker_wait,
+					 (task = lima_sched_pipe_get_task(pipe)) ||
+					 kthread_should_stop());
+
+		if (!task)
+			continue;
+
+		/* wait all dependent fence be signaled */
+		for (i = 0; i < task->num_dep; i++) {
+			/* interrupted by signal, may be kthread stop, ignore other error */
+			while (dma_fence_wait(task->dep[i], true) == -ERESTARTSYS) {
+				if (kthread_should_stop())
+					return 0;
+			}
+		}
+
+		if (!pipe->start_task(pipe->data, task)) {
+			while (dma_fence_wait(task->fence, true) == -ERESTARTSYS) {
+				if (kthread_should_stop())
+					return 0;
+			}
+		}
+
+		spin_lock_irqsave(&pipe->lock, saved_flags);
+		list_del(&task->list);
+		spin_unlock_irqrestore(&pipe->lock, saved_flags);
+		lima_sched_task_delete(task);
+		/* the only writer of this counter */
+		pipe->fence_done_seqno++;
+	}
+
+	return 0;
+}
+
+int lima_sched_pipe_init(struct lima_sched_pipe *pipe, const char *name)
+{
+	struct task_struct *worker;
+
 	pipe->name = name;
-	mutex_init(&pipe->lock);
+	spin_lock_init(&pipe->lock);
 	INIT_LIST_HEAD(&pipe->queue);
 	pipe->fence_context = dma_fence_context_alloc(1);
 	spin_lock_init(&pipe->fence_lock);
+	init_waitqueue_head(&pipe->worker_wait);
+
+	worker = kthread_run(lima_sched_pipe_worker, pipe, name);
+	if (IS_ERR(worker)) {
+		DRM_ERROR("Fail to create pipe worker for %s\n", name);
+		return PTR_ERR(worker);
+	}
+	pipe->worker = worker;
+	return 0;
 }
 
 void lima_sched_pipe_fini(struct lima_sched_pipe *pipe)
 {
 	struct lima_sched_task *task, *tmp;
 
+	kthread_stop(pipe->worker);
+
 	list_for_each_entry_safe(task, tmp, &pipe->queue, list) {
 		list_del(&task->list);
 		lima_sched_task_delete(task);
 	}
+}
+
+static unsigned long lima_timeout_to_jiffies(u64 timeout_ns)
+{
+	unsigned long timeout_jiffies;
+	ktime_t timeout;
+
+	/* clamp timeout if it's to large */
+	if (((s64)timeout_ns) < 0)
+		return MAX_SCHEDULE_TIMEOUT;
+
+	timeout = ktime_sub(ns_to_ktime(timeout_ns), ktime_get());
+	if (ktime_to_ns(timeout) < 0)
+		return 0;
+
+	timeout_jiffies = nsecs_to_jiffies(ktime_to_ns(timeout));
+	/*  clamp timeout to avoid unsigned-> signed overflow */
+	if (timeout_jiffies > MAX_SCHEDULE_TIMEOUT )
+		return MAX_SCHEDULE_TIMEOUT;
+
+	return timeout_jiffies;
+}
+
+static struct dma_fence *lima_sched_pipe_get_fence(struct lima_sched_pipe *pipe, u32 fence)
+{
+	unsigned long saved_flags;
+	struct lima_sched_task *task;
+	struct dma_fence *f = NULL;
+
+	spin_lock_irqsave(&pipe->lock, saved_flags);
+	list_for_each_entry(task, &pipe->queue, list) {
+		if (task->fence->seqno < fence)
+			continue;
+
+		if (task->fence->seqno == fence) {
+			f = task->fence;
+			dma_fence_get(f);
+		}
+
+		break;
+	}
+	spin_unlock_irqrestore(&pipe->lock, saved_flags);
+
+	return f;
+}
+
+int lima_sched_pipe_wait_fence(struct lima_sched_pipe *pipe, u32 fence, u64 timeout_ns)
+{
+	int ret = 0;
+
+	if (fence > pipe->fence_seqno)
+		return -EINVAL;
+
+	if (!timeout_ns)
+		return fence <= pipe->fence_done_seqno ? 0 : -EBUSY;
+	else {
+		unsigned long timeout = lima_timeout_to_jiffies(timeout_ns);
+		struct dma_fence *f = lima_sched_pipe_get_fence(pipe, fence);
+
+		if (f) {
+			ret = dma_fence_wait_timeout(f, true, timeout);
+			if (ret == 0)
+				ret = -ETIMEDOUT;
+			else if (ret > 0)
+				ret = 0;
+			dma_fence_put(f);
+		}
+	}
+
+	return ret;
 }
