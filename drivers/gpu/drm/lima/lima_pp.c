ac59362134adc920b912aff142aacf8ff26b095f
Qiang Yu <yuq825@gmail.com>
2017-05-02 14:26:22 +0800

    drm/lima: add wait fence ioctl
    Signed-off-by: Qiang Yu <yuq825@gmail.com>

--- drivers/gpu/drm/lima/lima_pp.c
+++ drivers/gpu/drm/lima/lima_pp.c
@@ -1,7 +1,18 @@
 #include "lima.h"
 
+static int lima_pp_start_task(void *data, struct lima_sched_task *task)
+{
+	struct lima_pp *pp = data;
+
+	DRM_INFO("lima start task pp %s\n", pp->ip.name);
+	dma_fence_signal(task->fence);
+	return 0;
+}
+
 int lima_pp_init(struct lima_pp *pp)
 {
+	pp->pipe.start_task = lima_pp_start_task;
+	pp->pipe.data = pp;
 	return 0;
 }
 
