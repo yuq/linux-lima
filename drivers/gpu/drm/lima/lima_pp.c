b312e7733590a15b5e78066725fad4dc42e439e6
Qiang Yu <yuq825@gmail.com>
2017-05-09 16:23:28 +0800

    drm/lima: real gp task finish dma fence signal
    Signed-off-by: Qiang Yu <yuq825@gmail.com>

--- drivers/gpu/drm/lima/lima_pp.c
+++ drivers/gpu/drm/lima/lima_pp.c
@@ -9,9 +9,18 @@ static int lima_pp_start_task(void *data, struct lima_sched_task *task)
 	return 0;
 }
 
+static int lima_pp_reset(void *data)
+{
+	struct lima_pp *pp = data;
+
+	DRM_INFO("lima reset pp %s\n", pp->ip.name);
+	return 0;
+}
+
 int lima_pp_init(struct lima_pp *pp)
 {
 	pp->pipe.start_task = lima_pp_start_task;
+	pp->pipe.reset = lima_pp_reset;
 	pp->pipe.data = pp;
 	pp->pipe.mmu = &pp->mmu;
 	return 0;
