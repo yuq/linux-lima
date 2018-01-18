32af551a8a05c4a58cfaa18360fb5024d08444a2
Qiang Yu <yuq825@gmail.com>
2018-01-18 22:26:05 +0800

    drm/lima: add gem_prime_res_obj callback
    Otherwise exported dma_buf won't share resv obj with
    device driver which import it.
    
    Signed-off-by: Qiang Yu <yuq825@gmail.com>

--- drivers/gpu/drm/lima/lima.h
+++ drivers/gpu/drm/lima/lima.h
@@ -157,6 +157,7 @@ struct drm_gem_object *lima_gem_prime_import_sg_table(struct drm_device *dev,
 						      struct dma_buf_attachment *attach,
 						      struct sg_table *sgt);
 struct sg_table *lima_gem_prime_get_sg_table(struct drm_gem_object *obj);
+struct reservation_object *lima_gem_prime_res_obj(struct drm_gem_object *obj);
 
 unsigned long lima_timeout_to_jiffies(u64 timeout_ns);
 
