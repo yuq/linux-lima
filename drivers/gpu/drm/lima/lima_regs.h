edcaf4042e8d8590b578b6f71c4346519e26af5d
Qiang Yu <yuq825@gmail.com>
2018-05-21 18:00:43 +0800

    drm/lima: use SPDX identifiers and change copyright
    Signed-off-by: Qiang Yu <yuq825@gmail.com>

--- drivers/gpu/drm/lima/lima_regs.h
+++ drivers/gpu/drm/lima/lima_regs.h
@@ -1,16 +1,6 @@
-/*
- * Copyright (C) 2010-2017 ARM Limited. All rights reserved.
- * Copyright (C) 2017-2018 Lima Project
- * 
- * This program is free software and is provided to you under
- * the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation, and any use by
- * you of this program is subject to the terms of such GNU
- * licence.
- * 
- * A copy of the licence is included with the program, and
- * can also be obtained from Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+/* Copyright 2010-2017 ARM Limited. All rights reserved.
+ * Copyright 2017-2018 Qiang Yu <yuq825@gmail.com>
+ * SPDX-License-Identifier: GPL-2.0
  */
 
 #ifndef __LIMA_REGS_H__
